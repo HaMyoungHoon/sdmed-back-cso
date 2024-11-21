cce497d48a1947d7e3c4ddab3921a43445975d30
하명훈 <basic_bansa@naver.com>
2024-11-21 14:29:03 +0900

    구조 변경으로 삭제

--- src/main/kotlin/sdmed/back/repository/sqlCSO/IUserSubDataRepository.kt
+++
@@ -1,9 +0,0 @@
-package sdmed.back.repository.sqlCSO
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import sdmed.back.model.sqlCSO.UserDataSubModel
-
-@Repository
-interface IUserSubDataRepository: JpaRepository<UserDataSubModel, Long> {
-}
