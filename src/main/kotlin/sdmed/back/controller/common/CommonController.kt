c16af27953a08d25466b89324b0bd9d0ad73b748
하명훈 <basic_bansa@naver.com>
2024-11-28 11:26:39 +0900

    lang set 추가

--- src/main/kotlin/sdmed/back/controller/common/CommonController.kt
+++ src/main/kotlin/sdmed/back/controller/common/CommonController.kt
@@ -1,14 +1,19 @@
 package sdmed.back.controller.common
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import sdmed.back.advice.exception.NotFoundLanguageException
 import sdmed.back.config.FConstants
 import sdmed.back.model.common.IRestResult
 import sdmed.back.service.AzureBlobService
 import sdmed.back.service.ResponseService
+import java.util.*
 
 @Tag(name = "CommonController")
 @RestController
@@ -20,6 +25,17 @@ class CommonController {
 	@Value(value = "\${str.version}") lateinit var strVersion: String
 	@Value(value = "\${str.profile}") lateinit var strprofile: String
 
+	@Operation(summary = "language set", description = "")
+	@PostMapping(value = ["/lang"])
+	fun setLanguage(@Parameter(name = "lang") @RequestParam lang: String): IRestResult {
+		when (lang) {
+			"ko" -> LocaleContextHolder.setDefaultLocale(Locale.KOREA)
+			"en" -> LocaleContextHolder.setDefaultLocale(Locale.ENGLISH)
+			else -> throw NotFoundLanguageException()
+		}
+
+		return responseService.getSuccessResult()
+	}
 	@GetMapping(value = ["/version"])
 	fun version(): IRestResult {
 		return responseService.getResult("$strprofile $strVersion")
