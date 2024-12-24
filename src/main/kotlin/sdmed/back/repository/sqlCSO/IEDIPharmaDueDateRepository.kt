935bf70ada163d821467db8552137693d9a75aaf
HaMyoungHoon <basicouts@gmail.com>
2024-12-24 15:13:54 +0900

    due date list range 로 변경

--- src/main/kotlin/sdmed/back/repository/sqlCSO/IEDIPharmaDueDateRepository.kt
+++ src/main/kotlin/sdmed/back/repository/sqlCSO/IEDIPharmaDueDateRepository.kt
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import sdmed.back.model.sqlCSO.edi.EDIPharmaDueDateModel
+import sdmed.back.model.sqlCSO.pharma.PharmaModel
+import java.util.Date
 
 @Repository
 interface IEDIPharmaDueDateRepository: JpaRepository<EDIPharmaDueDateModel, String> {
+	fun findByThisPK(thisPK: String): EDIPharmaDueDateModel?
+
 	@Query("SELECT a FROM EDIPharmaDueDateModel a " +
 			"WHERE a.year = :year " +
 			"ORDER BY a.year, a.month DESC")
@@ -15,6 +19,16 @@ interface IEDIPharmaDueDateRepository: JpaRepository<EDIPharmaDueDateModel, Stri
 			"WHERE a.year = :year AND a.month = :month " +
 			"ORDER BY a.year, a.month DESC")
 	fun selectAllByThisYearMonthDueDate(year: String, month: String): List<EDIPharmaDueDateModel>
+	@Query("SELECT a.* FROM EDIPharmaDueDateModel a " +
+			"WHERE CONVERT(DATE, CONCAT(a.year, '-', a.month, '-', a.day)) BETWEEN :startDate AND :endDate " +
+			"ORDER BY a.year, a.month DESC", nativeQuery = true)
+	fun selectAllByThisYearMonthRangeDueDate(startDate: Date, endDate: Date): List<EDIPharmaDueDateModel>
+	@Query("SELECT a FROM PharmaModel a " +
+			"WHERE a.inVisible = :inVisible AND a.thisPK NOT IN (" +
+			"SELECT b.pharmaPK FROM EDIPharmaDueDateModel b " +
+			"WHERE b.year = :year AND b.month = :month) " +
+			"ORDER BY a.code DESC")
+	fun selectPharmaListByThisYearMonthDueDate(year: String, month: String, inVisible: Boolean = false): List<PharmaModel>
 	@Query("SELECT a FROM EDIPharmaDueDateModel a " +
 			"WHERE a.pharmaPK = :pharmaPK AND a.year = :year " +
 			"ORDER BY a.year, a.month DESC")
