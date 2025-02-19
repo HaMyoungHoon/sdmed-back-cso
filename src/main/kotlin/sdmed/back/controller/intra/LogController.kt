8409ff18aefb731d0ea9bc0c4f4d908ed7ccbbc7
HaMyoungHoon <basicouts@gmail.com>
2025-02-20 08:16:59 +0900

    오타 수정

--- src/main/kotlin/sdmed/back/controller/intra/LogController.kt
+++ src/main/kotlin/sdmed/back/controller/intra/LogController.kt
@@ -3,6 +3,7 @@ package sdmed.back.controller.intra
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -10,14 +11,14 @@ import org.springframework.web.bind.annotation.RestController
 import sdmed.back.config.FControllerBase
 import sdmed.back.service.CommonService
 
-@Tag(name = "intra EDI 체크리스트")
+@Tag(name = "intra log")
 @RestController
 @RequestMapping(value = ["/intra/log"])
 class LogController: FControllerBase() {
 	@Autowired lateinit var commonService: CommonService
 
 	@Operation(summary = "get log view model")
-	@RequestMapping(value = ["/list"])
+	@GetMapping(value = ["/list"])
 	fun getList(@RequestHeader token: String,
 							@RequestParam(required = false) page: Int = 0,
 							@RequestParam(required = false) size: Int = 1000) =
