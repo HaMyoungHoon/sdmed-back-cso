510afb2c5b02cf25eb3456eaeeb3641ec4c4e4fa
하명훈 <basicouts@gmail.com>
2025-03-20 17:59:00 +0900

    제약사 데이터베이스 변경

--- src/main/kotlin/sdmed/back/model/sqlCSO/pharma/PharmaModel.kt
+++ src/main/kotlin/sdmed/back/model/sqlCSO/pharma/PharmaModel.kt
@@ -3,12 +3,8 @@ package sdmed.back.model.sqlCSO.pharma
 import jakarta.persistence.*
 import sdmed.back.config.FConstants
 import sdmed.back.config.FExtensions
-import sdmed.back.model.common.*
-import sdmed.back.model.common.pharma.PharmaGroup
-import sdmed.back.model.common.pharma.PharmaType
 import sdmed.back.model.sqlCSO.FExcelParseModel
 import sdmed.back.model.sqlCSO.medicine.MedicineModel
-import java.lang.StringBuilder
 import java.sql.Date
 import java.util.*
 
@@ -28,54 +24,26 @@ data class PharmaModel(
 	@Column(columnDefinition = "nvarchar(255)", nullable = false)
 	var taxpayerNumber: String = "",
 	@Column(columnDefinition = "nvarchar(255)", nullable = false)
+	var address: String = "",
+	@Column(columnDefinition = "nvarchar(255)", nullable = false)
 	var phoneNumber: String = "",
 	@Column(columnDefinition = "nvarchar(255)", nullable = false)
 	var faxNumber: String = "",
 	@Column(columnDefinition = "nvarchar(100)", nullable = false)
 	var zipCode: String = "",
-	@Column(columnDefinition = "nvarchar(255)", nullable = false)
-	var address: String = "",
-	@Column(columnDefinition = "nvarchar(255)", nullable = false)
-	var addressDetail: String = "",
-	@Column(columnDefinition = "nvarchar(255)", nullable = false)
-	var businessType: String = "",
-	@Column(columnDefinition = "nvarchar(255)", nullable = false)
-	var businessItem: String = "",
-	@Column(nullable = false)
-	var billType: BillType = BillType.None,
-	@Column(nullable = false)
-	var pharmaType: PharmaType = PharmaType.None,
-	@Column(nullable = false)
-	var pharmaGroup: PharmaGroup = PharmaGroup.None,
-	@Column(nullable = false)
-	var contractType: ContractType = ContractType.None,
-	@Column(nullable = false)
-	var deliveryDiv: DeliveryDiv = DeliveryDiv.None,
-	@Column(columnDefinition = "nvarchar(255)", nullable = false)
-	var mail: String = "",
-	@Column(columnDefinition = "nvarchar(255)", nullable = false)
-	var mobilePhone: String = "",
 	@Column
 	var openDate: Date? = null,
-	@Column
-	var closeDate: Date? = null,
 	@Column(columnDefinition = "text", nullable = false)
 	var retroactiveRule: String = "",
 	@Column(columnDefinition = "text", nullable = false)
 	var innerSettlementRule: String = "",
 	@Column(columnDefinition = "text", nullable = false)
 	var outerSettlementRule: String = "",
-	// mysql
 	@Column(columnDefinition = "text", nullable = false)
-//	@Column(columnDefinition = "nvarchar(max)", nullable = false)
 	var etc1: String = "",
-	// mysql
 	@Column(columnDefinition = "text", nullable = false)
-//	@Column(columnDefinition = "nvarchar(max)", nullable = false)
 	var etc2: String = "",
-	// mysql
 	@Column(columnDefinition = "text", nullable = false)
-//	@Column(columnDefinition = "nvarchar(max)", nullable = false)
 	var imageUrl: String = "",
 	@Column(columnDefinition = "bit default 0", nullable = false)
 	var inVisible: Boolean = false,
@@ -93,27 +61,16 @@ data class PharmaModel(
 			2 -> innerName
 			3 -> ownerName
 			4 -> taxpayerNumber
-			5 -> phoneNumber
-			6 -> faxNumber
-			7 -> zipCode
-			8 -> address
-			9 -> addressDetail
-			10 -> businessType
-			11 -> businessItem
-			12 -> billType.desc
-			13 -> pharmaType.desc
-			14 -> pharmaGroup.desc
-			15 -> contractType.desc
-			16 -> deliveryDiv.desc
-			17 -> mail
-			18 -> mobilePhone
-			19 -> FExtensions.parseDateTimeString(openDate, "yyyy-MM-dd")
-			20 -> FExtensions.parseDateTimeString(closeDate, "yyyy-MM-dd")
-			21 -> retroactiveRule
-			22 -> innerSettlementRule
-			23 -> outerSettlementRule
-			24 -> etc1
-			25 -> etc2
+			5 -> address
+			6 -> phoneNumber
+			7 -> faxNumber
+			8 -> zipCode
+			9 -> FExtensions.parseDateTimeString(openDate, "yyyy-MM-dd")
+			10 -> retroactiveRule
+			11 -> innerSettlementRule
+			12 -> outerSettlementRule
+			13 -> etc1
+			14 -> etc2
 			else -> ""
 		}
 	}
@@ -128,23 +85,12 @@ data class PharmaModel(
 			6 -> faxNumber = data ?: ""
 			7 -> zipCode = data ?: ""
 			8 -> address = data ?: ""
-			9 -> addressDetail = data ?: ""
-			10 -> businessType = data ?: ""
-			11 -> businessItem = data ?: ""
-			12 -> billType = BillType.parseString(data)
-			13 -> pharmaType = PharmaType.parseString(data)
-			14 -> pharmaGroup = PharmaGroup.parseString(data)
-			15 -> contractType = ContractType.parseString(data)
-			16 -> deliveryDiv = DeliveryDiv.parseString(data)
-			17 -> mail = data ?: ""
-			18 -> mobilePhone = data ?: ""
-			19 -> openDate = if (data.isNullOrEmpty()) null else FExtensions.parseStringToSqlDate(data, "yyyy-MM-dd")
-			20 -> closeDate = if (data.isNullOrEmpty()) null else FExtensions.parseStringToSqlDate(data, "yyyy-MM-dd")
-			21 -> retroactiveRule = data ?: ""
-			22 -> innerSettlementRule = data ?: ""
-			23 -> outerSettlementRule = data ?: ""
-			24 -> etc1 = data ?: ""
-			25 -> etc2 = data ?: ""
+			9 -> openDate = if (data.isNullOrEmpty()) null else FExtensions.parseStringToSqlDate(data, "yyyyMMdd")
+			10 -> retroactiveRule = data ?: ""
+			11 -> innerSettlementRule = data ?: ""
+			12 -> outerSettlementRule = data ?: ""
+			13 -> etc1 = data ?: ""
+			14 -> etc2 = data ?: ""
 		}
 	}
 	override fun titleGet(index: Int): String {
@@ -154,27 +100,16 @@ data class PharmaModel(
 			2 -> FConstants.MODEL_INNER_NAME
 			3 -> FConstants.MODEL_OWNER_NAME
 			4 -> FConstants.MODEL_TAX_PAYER
-			5 -> FConstants.MODEL_PHONE
-			6 -> FConstants.MODEL_FAX
-			7 -> FConstants.MODEL_ZIP_CODE
-			8 -> FConstants.MODEL_ADDRESS
-			9 -> FConstants.MODEL_ADDRESS_DETAIL
-			10 -> FConstants.MODEL_BIZ_TYPE
-			11 -> FConstants.MODEL_BIZ_ITEM
-			12 -> FConstants.MODEL_BILL_TYPE
-			13 -> FConstants.MODEL_CO_TYPE
-			14 -> FConstants.MODEL_CO_GROUP
-			15 -> FConstants.MODEL_CONTRACT_TYPE
-			16 -> FConstants.MODEL_DELIVERY
-			17 -> FConstants.MODEL_MAIL
-			18 -> FConstants.MODEL_MOBILE_PHONE
-			19 -> FConstants.MODEL_OPEN_DATE
-			20 -> FConstants.MODEL_CLOSE_DATE
-			21 -> FConstants.MODEL_RETROACTIVE_RULE
-			22 -> FConstants.MODEL_INNER_SETTLEMENT_RULE
-			23 -> FConstants.MODEL_OUTER_SETTLEMENT_RULE
-			24 -> FConstants.MODEL_ETC1
-			25 -> FConstants.MODEL_ETC2
+			5 -> FConstants.MODEL_ADDRESS
+			6 -> FConstants.MODEL_PHONE
+			7 -> FConstants.MODEL_FAX
+			8 -> FConstants.MODEL_ZIP_CODE
+			9 -> FConstants.MODEL_OPEN_DATE
+			10 -> FConstants.MODEL_RETROACTIVE_RULE
+			11 -> FConstants.MODEL_INNER_SETTLEMENT_RULE
+			12 -> FConstants.MODEL_OUTER_SETTLEMENT_RULE
+			13 -> FConstants.MODEL_ETC1
+			14 -> FConstants.MODEL_ETC2
 			else -> ""
 		}
 	}
@@ -185,14 +120,6 @@ data class PharmaModel(
 			return true
 		} else if (indexGet(2).isEmpty()) {
 			return true
-//		} else if (getIndex(3).isEmpty()) {
-//			return true
-//		} else if (getIndex(4).isEmpty()) {
-//			return true
-//		} else if (getIndex(7).isEmpty()) {
-//			return true
-//		} else if (getIndex(8).isEmpty()) {
-//			return true
 		}
 		return false
 	}
@@ -212,19 +139,13 @@ data class PharmaModel(
 		val faxNumber = FExtensions.escapeString(faxNumber)
 		val zipCode = FExtensions.escapeString(zipCode)
 		val address = FExtensions.escapeString(address)
-		val addressDetail = FExtensions.escapeString(addressDetail)
-		val businessType = FExtensions.escapeString(businessType)
-		val businessItem = FExtensions.escapeString(businessItem)
-		val mail = FExtensions.escapeString(mail)
-		val mobilePhone = FExtensions.escapeString(mobilePhone)
 		val openDateString: String = openDate?.let { "'${FExtensions.parseDateTimeString(it, "yyyy-MM-dd")}'" } ?: "null"
-		val closeDateString: String = closeDate?.let { "'${FExtensions.parseDateTimeString(it, "yyyy-MM-dd")}'" } ?: "null"
 		val retroactiveRule = FExtensions.escapeString(retroactiveRule)
 		val innerSettlementRule = FExtensions.escapeString(innerSettlementRule)
 		val outerSettlementRule = FExtensions.escapeString(outerSettlementRule)
 		val etc1 = FExtensions.escapeString(etc1)
 		val etc2 = FExtensions.escapeString(etc2)
-		return "('$thisPK', '$code', '$orgName', '$innerName', '$ownerName', '$taxpayerNumber', '$phoneNumber', '$faxNumber', '$zipCode', '$address', '$addressDetail', '$businessType', '$businessItem', '${billType.index}', '${pharmaType.index}', '${pharmaGroup.index}', '${contractType.index}', '${deliveryDiv.index}', '$mail', '$mobilePhone', $openDateString, $closeDateString, '$retroactiveRule', '$innerSettlementRule', '$outerSettlementRule', '$etc1', '$etc2', '$imageUrl', ${if (inVisible) 1 else 0})"
+		return "('$thisPK', '$code', '$orgName', '$innerName', '$ownerName', '$taxpayerNumber', '$address', '$phoneNumber', '$faxNumber', '$zipCode', $openDateString, '$retroactiveRule', '$innerSettlementRule', '$outerSettlementRule', '$etc1', '$etc2', '$imageUrl', ${if (inVisible) 1 else 0})"
 	}
 	fun safeCopy(rhs: PharmaModel): PharmaModel {
 		this.orgName = rhs.orgName
@@ -235,18 +156,7 @@ data class PharmaModel(
 		this.faxNumber = rhs.faxNumber
 		this.zipCode = rhs.zipCode
 		this.address = rhs.address
-		this.addressDetail = rhs.addressDetail
-		this.businessType = rhs.businessType
-		this.businessItem = rhs.businessItem
-		this.billType = rhs.billType
-		this.pharmaType = rhs.pharmaType
-		this.pharmaGroup = rhs.pharmaGroup
-		this.contractType = rhs.contractType
-		this.deliveryDiv = rhs.deliveryDiv
-		this.mail = rhs.mail
-		this.mobilePhone = rhs.mobilePhone
 		this.openDate = rhs.openDate
-		this.closeDate = rhs.closeDate
 		this.retroactiveRule = rhs.retroactiveRule
 		this.innerSettlementRule = rhs.innerSettlementRule
 		this.outerSettlementRule = rhs.outerSettlementRule
