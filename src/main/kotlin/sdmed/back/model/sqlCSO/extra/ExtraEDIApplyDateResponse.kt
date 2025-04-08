cd4324a059f9de9dcca0b05465fa4c8fdbea7c06
하명훈 <basicouts@gmail.com>
2025-04-09 08:03:08 +0900

    구분자 추가

--- src/main/kotlin/sdmed/back/model/sqlCSO/extra/ExtraEDIApplyDateResponse.kt
+++ src/main/kotlin/sdmed/back/model/sqlCSO/extra/ExtraEDIApplyDateResponse.kt
@@ -1,6 +1,9 @@
 package sdmed.back.model.sqlCSO.extra
 
+import java.util.UUID
+
 data class ExtraEDIApplyDateResponse(
+    var thisPK: String = UUID.randomUUID().toString(),
     var year: String = "",
     var month: String = "",
 ) {
